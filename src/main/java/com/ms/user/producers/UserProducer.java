ba826dc663b6e26fdb130e39df18e312c4b71cbc
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-08-12 21:39:30 -0300

    Configure RabbitMQ and create producer

---
+++ src/main/java/com/ms/user/producers/UserProducer.java
@@ -0,0 +1,32 @@
+package com.ms.user.producers;
+
+import com.ms.user.dtos.EmailDto;
+import com.ms.user.models.UserModel;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value(value = "${spring.rabbitmq.queue}")
+    private String routingKey;
+
+    public void publishMessageEmail(UserModel userModel){
+        var emailDto = new EmailDto();
+
+        emailDto.setUserId(userModel.getUserId());
+        emailDto.setEmailTo(userModel.getEmail());
+        emailDto.setSubject("registration completed successfully!");
+        emailDto.setText(userModel.getName() + ", welcome! \nThank you for registering, enjoy all the features of our platform now!");
+
+        rabbitTemplate.convertAndSend("", routingKey, emailDto);
+    }
+
+}
